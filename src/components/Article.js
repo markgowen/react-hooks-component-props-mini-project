23fc85971e0cc52a7eff3d11a9c8182d08e49769
Mark Gowen <mark.gowen78@gmail.com>
2022-11-25 10:40:41 -0800

    Bonus feature completed

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,21 @@
 import React from "react";
 
-function Article({ title, date = "January 1, 1970", preview}) {
-
+function makeEmojis(minutes) {
+    const emoji = minutes < 30 ? "☕️" : "🍱"
+    const interval = minutes < 30 ? 5 : 10
+    
+    let emojis = "";
+    for (let i = 0; i < minutes; i += interval) {
+        emojis += emoji;
+    }
+    return emojis;
+}
+function Article({ title, date = "January 1, 1970", preview, minutes }) {
+    const emojis = makeEmojis(minutes)
     return (
         <article>
             <h3>{title}</h3>
-            <small>{date}</small>
+            <small>{date} {emojis} {minutes} min read</small>
             <p>{preview}</p>
         </article>
     );
